d4e5c50ad63cfcc007fe97e5119f7fe4553c9608
Stardurost <88589361+Stardurost@users.noreply.github.com>
2021-10-19 16:17:04 +0300

    Add files via upload

--- LR15-16/SisProgr16/SisProgr16/MyForm.h
+++ LR15-16/SisProgr16/SisProgr16/MyForm.h
@@ -1,4 +1,5 @@
 #pragma once
+#include<stdlib.h>
 
 namespace SisProgr16 {
 
@@ -42,12 +43,13 @@ namespace SisProgr16 {
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::TextBox^ textBox2;
 	private: System::Windows::Forms::TextBox^ textBox3;
-	private: System::Windows::Forms::TextBox^ textBox4;
-	private: System::Windows::Forms::TextBox^ textBox5;
-	private: System::Windows::Forms::TextBox^ textBox6;
-	private: System::Windows::Forms::Button^ button2;
+
+
+
+
 	private: System::Windows::Forms::TextBox^ textBox7;
 	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
 
 	private:
 		/// <summary>
@@ -70,18 +72,16 @@ namespace SisProgr16 {
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
-			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
 			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// textBox1
 			// 
-			this->textBox1->BackColor = System::Drawing::Color::LightPink;
-			this->textBox1->Location = System::Drawing::Point(251, 30);
+			this->textBox1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->textBox1->Location = System::Drawing::Point(189, 30);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(100, 20);
 			this->textBox1->TabIndex = 0;
@@ -89,12 +89,13 @@ namespace SisProgr16 {
 			// button1
 			// 
 			this->button1->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
-			this->button1->BackColor = System::Drawing::Color::LavenderBlush;
+			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(224)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)));
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button1->Location = System::Drawing::Point(30, 218);
+			this->button1->Location = System::Drawing::Point(104, 218);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(129, 41);
+			this->button1->Size = System::Drawing::Size(139, 41);
 			this->button1->TabIndex = 3;
 			this->button1->Text = L"Рассчитать";
 			this->button1->UseVisualStyleBackColor = false;
@@ -106,9 +107,10 @@ namespace SisProgr16 {
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11));
 			this->label1->Location = System::Drawing::Point(27, 32);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(157, 18);
+			this->label1->Size = System::Drawing::Size(78, 18);
 			this->label1->TabIndex = 4;
-			this->label1->Text = L"Введите километраж";
+			this->label1->Text = L"Введите а";
+			
 			// 
 			// label2
 			// 
@@ -116,9 +118,9 @@ namespace SisProgr16 {
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11));
 			this->label2->Location = System::Drawing::Point(27, 168);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(162, 18);
+			this->label2->Size = System::Drawing::Size(73, 18);
 			this->label2->TabIndex = 5;
-			this->label2->Text = L"Введите цену топлива";
+			this->label2->Text = L"Введите l";
 			// 
 			// label3
 			// 
@@ -126,94 +128,66 @@ namespace SisProgr16 {
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11));
 			this->label3->Location = System::Drawing::Point(27, 98);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(180, 18);
+			this->label3->Size = System::Drawing::Size(78, 18);
 			this->label3->TabIndex = 6;
-			this->label3->Text = L"Введите расход топлива";
+			this->label3->Text = L"Введите b";
 			// 
 			// textBox2
 			// 
-			this->textBox2->BackColor = System::Drawing::Color::LightPink;
-			this->textBox2->Location = System::Drawing::Point(251, 99);
+			this->textBox2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->textBox2->Location = System::Drawing::Point(189, 96);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(100, 20);
 			this->textBox2->TabIndex = 7;
 			// 
 			// textBox3
 			// 
-			this->textBox3->BackColor = System::Drawing::Color::LightPink;
-			this->textBox3->Location = System::Drawing::Point(251, 169);
+			this->textBox3->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->textBox3->Location = System::Drawing::Point(189, 166);
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(100, 20);
 			this->textBox3->TabIndex = 8;
 			// 
-			// textBox4
-			// 
-			this->textBox4->BackColor = System::Drawing::Color::LightPink;
-			this->textBox4->Location = System::Drawing::Point(402, 30);
-			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(100, 20);
-			this->textBox4->TabIndex = 9;
-			// 
-			// textBox5
-			// 
-			this->textBox5->BackColor = System::Drawing::Color::LightPink;
-			this->textBox5->Location = System::Drawing::Point(402, 99);
-			this->textBox5->Name = L"textBox5";
-			this->textBox5->Size = System::Drawing::Size(100, 20);
-			this->textBox5->TabIndex = 10;
-			// 
-			// textBox6
-			// 
-			this->textBox6->BackColor = System::Drawing::Color::LightPink;
-			this->textBox6->Location = System::Drawing::Point(402, 169);
-			this->textBox6->Name = L"textBox6";
-			this->textBox6->Size = System::Drawing::Size(100, 20);
-			this->textBox6->TabIndex = 11;
-			// 
-			// button2
-			// 
-			this->button2->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
-			this->button2->BackColor = System::Drawing::Color::LavenderBlush;
-			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->button2->Location = System::Drawing::Point(288, 218);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(214, 41);
-			this->button2->TabIndex = 12;
-			this->button2->Text = L"Ввести тестовые значения";
-			this->button2->UseVisualStyleBackColor = false;
-			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
-			// 
 			// textBox7
 			// 
-			this->textBox7->BackColor = System::Drawing::Color::LightPink;
-			this->textBox7->Location = System::Drawing::Point(30, 327);
+			this->textBox7->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->textBox7->Location = System::Drawing::Point(104, 312);
 			this->textBox7->Name = L"textBox7";
-			this->textBox7->Size = System::Drawing::Size(472, 20);
+			this->textBox7->Size = System::Drawing::Size(139, 20);
 			this->textBox7->TabIndex = 13;
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11));
-			this->label4->Location = System::Drawing::Point(180, 286);
+			this->label4->Location = System::Drawing::Point(89, 273);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(168, 18);
 			this->label4->TabIndex = 14;
 			this->label4->Text = L"Результат вычисления";
 			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11));
+			this->label5->Location = System::Drawing::Point(65, 312);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(35, 18);
+			this->label5->TabIndex = 15;
+			this->label5->Text = L"S = ";
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::Color::Pink;
-			this->ClientSize = System::Drawing::Size(533, 359);
+			this->BackColor = System::Drawing::Color::PeachPuff;
+			this->ClientSize = System::Drawing::Size(333, 359);
+			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->textBox7);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->textBox6);
-			this->Controls->Add(this->textBox5);
-			this->Controls->Add(this->textBox4);
 			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->label3);
@@ -224,39 +198,38 @@ namespace SisProgr16 {
 			this->Cursor = System::Windows::Forms::Cursors::Cross;
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"MyForm";
-			this->Text = L"Вычисление стоимость поездки";
+			this->Text = L"Вычисление";
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-		double s2, a2, b2;
-		s2 = 240; a2 = 8.4; b2 = 16.5;
-		textBox1->Text = s2.ToString();
-		textBox2->Text = a2.ToString();
-		textBox3->Text = b2.ToString();
-		double result = s2*a2*b2/ 100;
-		textBox7->Text = result.ToString();
-	}
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-	double s, a, b, s1, a1, b1;
-	s = Convert::ToDouble(textBox1->Text);
-	a = Convert::ToDouble(textBox2->Text);
-	b = Convert::ToDouble(textBox3->Text);
-	double result = s*a*b/100;
-	if (textBox4->Text == "" || textBox5->Text == "" || textBox6->Text == "")
+	double a, b, l;
+	if (textBox1->Text == "" || textBox2->Text == "" || textBox3->Text == ""  )
 	{
-		textBox7->Text = result.ToString();
+		MessageBox::Show("Заполните, пожалуйста, данные", "Ошибка ввода данных", MessageBoxButtons::OK,
+			MessageBoxIcon::Exclamation);
 	}
 	else {
-		s1 = Convert::ToDouble(textBox4->Text);
-		a1 = Convert::ToDouble(textBox5->Text);
-		b1 = Convert::ToDouble(textBox6->Text);
-		double result1 = s1 * a1 * b1 / 100;
-		double sum = result + result1;
-		textBox7->Text = sum.ToString();
+		l = Convert::ToDouble(textBox3->Text);
+		a = Convert::ToDouble(textBox1->Text);
+		b = Convert::ToDouble(textBox2->Text);
+		double s = 34 / a + 12 + l / (b * b);
+		textBox7->Text = s.ToString();
+	}
+	if(textBox1->Text == "0" || textBox2->Text == "0") {
+		MessageBox::Show("Деление на ноль !", "Ошибка ввода данных", MessageBoxButtons::OK,
+			MessageBoxIcon::Exclamation);
+	}
+	else {
+		l = Convert::ToDouble(textBox3->Text);
+		a = Convert::ToDouble(textBox1->Text);
+		b = Convert::ToDouble(textBox2->Text);
+		double s = 34 / a + 12 + l / (b * b);
+		textBox7->Text = s.ToString();
 	}
 }
+
 };
 }
